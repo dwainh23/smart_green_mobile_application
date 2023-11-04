5c8f5db03e433ebeed4c8b28698c2b38f4c76e52
dana showaiter <61546844+dwainh23@users.noreply.github.com>
2023-11-04 05:02:17 +0300

    smart green 4/11/2023-1

--- lib/main.dart
+++ lib/main.dart
@@ -2,29 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:smart_green_mobile_application/pages/splash_screen/splash_screen.dart';
 
 void main() {
-  runApp(const SmartGreenApp());
+  runApp(const MyApp());
 }
-class SmartGreenApp extends StatelessWidget {
-  const SmartGreenApp({super.key});
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-/*      theme: ThemeData(
-        brightness: Brightness.light,
-        primarySwatch: const MaterialColor(0xFF36502d,<int, Color>{
-          50: Color(0x9836502d),
-          100: Color(0xb336502d),
-          200: Color(0xcd36502d),
-          300: Color(0xe536502d),
-          400: Color(0xFF36502d),
-        }),
-
-      ) ,
-      themeMode: ThemeMode.system,
-      */
-      home:SpalshScreen(),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const SplashScreen(),
     );
   }
 }
-
--- lib/pages/splash_screen/splash_screen.dart
+++ lib/pages/splash_screen/splash_screen.dart
@@ -1,24 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:/assets/images/images_strings.dart'as tp;
 
-class SpalshScreen extends StatelessWidget {
-  const SpalshScreen({super.key});
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Stack(
-        children: [
-          Positioned(
-            top:10,
-            left:10,
-            child:Image(
-              image: AssetImage(tp.topSplashIcon),width: 50,height: 50,
-              ),
-          )
-        ],
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned(
+              top:0,
+                left: 10,
+                child: Image(
+                  image: AssetImage('assets/icons/smartGreenIcon_1.png'),height: 100,width: 100,
+                ),
+            ),
+            Positioned(
+              top:110,
+              left: 30,
+              child: Column(
+                children: [
+                  Text("Smart Green", ),
+                  Text("Welcome To Smart Green\n Where You can \nBetter Take Care\nof \nYour Plants")
+                ],
+              )
+            ),
+          ],
+        ),
+      ),
 
-      )
     );
   }
 }
